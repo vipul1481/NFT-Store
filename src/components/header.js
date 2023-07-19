8d0d7ca7bd66c7a2e8d66b8ea1f5e1b6b5e1f602
Vipul Goyal <vipulgoyal1481@gmail.com>
2023-07-19 15:23:47 +0530

    made the header structure

---
+++ src/components/header.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './header.css'
+import punkLogo from '../assets/header/cryptopunk-logo.png'
+import searchIcon from '../assets/header/search.png'
+import themeSwitchIcon from '../assets/header/theme-switch.png'
+const header = () => {
+  return (
+   <div className='head'>
+
+      <div className='logocontainer'>
+        <img src={punkLogo} className='punkLogo' alt=''/>
+      </div>
+
+      <div className='searchBar'>
+        <div className='searchIconContainer'>
+            <img src={searchIcon} />
+        </div>
+        <input className='searchInput' placeholder='Collection, item or user...'/>
+      </div>
+
+      <div className='headerItems'>
+        <p>Drops</p>
+        <p>Marketplace</p>
+        <p>Create</p>
+      </div>
+
+      <div className='headerActions'>
+        <div className='themeSwitchContainer'>
+            <img src={themeSwitchIcon} alt=''/>
+        </div>
+      </div>
+
+      <div className='loginButton'>GET IN</div>
+
+
+
+   </div>
+
+  
+ ) 
+}
+
+export default header
